c3dc68b98c62ca9496048a966bbe7f6737390271
BassMasterTT <tothtibi22@gmail.com>
2021-02-20 01:00:45 +0800

    destructuring props

--- src/index.js
+++ src/index.js
@@ -2,38 +2,45 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
+const firstBook = {
+  img:
+    'https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY327_FMwebp_QL65_.jpg',
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth',
+};
+
+const secondBook = {
+  img:
+    'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY327_FMwebp_QL65_.jpg',
+  title: 'Our Class is a Family',
+  author: 'Shannon Olsen',
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt="book cover" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    className="image"
-    src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY327_FMwebp_QL65_.jpg"
-    alt="book cover"
-  />
-);
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => <h4>Amelia Hepworth</h4>;
 
 ReactDom.render(<BookList />, document.getElementById('root'));
