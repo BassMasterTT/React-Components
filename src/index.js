2fe2b0fdcc088685dde814b637eb87f709cf0693
BassMasterTT <tothtibi22@gmail.com>
2021-02-19 21:53:33 +0800

    BookList component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
-function Greeting() {
-  return <h4>This is Tibor and this is may first component</h4>;
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY327_FMwebp_QL65_.jpg"
+    alt="book cover"
+  />
+);
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => <h4>Amelia Hepworth</h4>;
+
+ReactDom.render(<BookList />, document.getElementById('root'));
